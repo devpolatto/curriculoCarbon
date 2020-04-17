9aca5488f6d0ee6e773b9122423780dbd46f7866
devpolattogit config --global user.name devpolatto <polatto.xcode.job@gmail.com>
2020-04-16 21:38:46 -0300

    curriculo howIAm 1.0

---
+++ howIAm.js
@@ -0,0 +1,53 @@
+function aboutMe() {
+    const name = "Angelo Vinicius Marinho Polatto"
+    const contact = {
+        Email: "polatto.xcode.job@gmail.com",
+        Phone: "(81) 99976-6539",
+        Linkedin: "www.linkedin.com/in/angelo-polatto-04121998",
+    };
+
+    let education = [
+        {
+            course: "Redes de Computadores",
+            institution: "Centro Universitário Brasileiro",
+            period: "02/2018 - 06/2020"
+        },
+    ];
+
+    let workExperience = [
+        {
+            company: "NIXCOMM",
+            office: "Técnico de TI",
+            period: "10/2019 - atualmente"
+        },
+        {
+            company: "Casa Sabor",
+            office: "Estágio em suporte TI",
+            period: "08/2019 - 10/2019"
+        },
+        {
+            company: "Centro Universitário Brasileiro - UNIBRA",
+            office: "Estágio em suporte TI",
+            period: "05/2018 - 10/2018"
+        }
+    ];
+
+    let skills = {
+        programmingLanguages: "JavaScript, Python",
+        markupStyles: "HTML5, CSS",
+        dataBases: "SQLite, MongoDB",
+        versionControl: "Git, GitHub",
+        frameworks: "Express, ReactJS",
+        firewall: "Fortigate",
+        accessPoint: "FortiAP, Cambium",
+        CFTV: "Hik-vision, Intelbras",
+        tools: "Photoshop, Insomnia"
+    };
+
+    let languages = [
+        {
+            language: "Inglês",
+            level: "intermediário"
+        }
+    ]
+}
